c6b9373b2ac896774ffa8d625c2d11564fd33f2b
npretorius <npretorius@nextflex.us>
2023-04-21 19:20:43 -0700

    simplified MotionController class

--- src/hardware/MotionController.py
+++ src/hardware/MotionController.py
@@ -15,10 +15,10 @@ def open_serial(port, baudrate):
     return device
 
 class MotionController:
-    def __init__(self, port='/dev/ttyACM0', baudrate=115200, test_mode=True):
+    def __init__(self, serial_port='/dev/ttyACM0', baudrate=115200, test_mode=True):
         self.test_mode=test_mode
         self.baudrate = baudrate
-        self.port = port
+        self.serial_port = serial_port
         self.device = None
         self.current_position = None
 
@@ -28,21 +28,19 @@ class MotionController:
             return True
         else:
             try:
-                self.device = open_serial(self.port, self.baudrate)
+                self.device = open_serial(self.serial_port, self.baudrate)
                 return True
             except:
                 return False
-                #sys.exit(0)
-
-
-    # wrapper function for connect() that prints out start banner and has delay.
+    
     def start(self):
+        """wrapper function for connect() that prints out start banner and has delay."""
         self.connect()
         message = self.read()
         return message
 
-    # Sends a message to the device and returns the response message
     def send(self, message):
+        """Sends a message to the device and returns the response message"""
         if self.test_mode:
             response="ok\n"
             return response
@@ -65,61 +63,61 @@ class MotionController:
             response += self.device.readline().decode()
         return response
 
-    def get_absolute_position(self):
-        if self.test_mode:
-            return self.current_position
-        else:
-            # get the current absolute position of print head
-            return self.parsePosition(self.send('M114'))
 
-    def parsePosition(self, line):
-        res = line.split(' ') 
-        logging.info("response: {}".format(res))
+    # def get_absolute_position(self):
+    #     if self.test_mode:
+    #         return self.current_position
+    #     else:
+    #         # get the current absolute position of print head
+    #         return self.parsePosition(self.send('M114'))
+
+    # def parsePosition(self, line):
+    #     res = line.split(' ') 
+    #     logging.info("response: {}".format(res))
         
-        # Only get the X, Y, and Z
-        try:
-            locations = [float(item[2:]) for item in res[0:3]] 
-            logging.info("locations: {}".format(locations))
-            return locations
-        except:
-            return None
+    #     # Only get the X, Y, and Z
+    #     try:
+    #         locations = [float(item[2:]) for item in res[0:3]] 
+    #         logging.info("locations: {}".format(locations))
+    #         return locations
+    #     except:
+    #         return None
 
-    # perform homing action
-    def home(self):
-        if self.test_mode:
-            self.current_position = [0,0,0] 
-        return self.send('G28') 
+    # def home(self):
+    #     if self.test_mode:
+    #         self.current_position = [0,0,0] 
+    #     return self.send('G28') 
 
-    def homeXY(self):
-        if self.test_mode:
-            self.current_position[0] = 0.0 
-            self.current_position[1] = 0.0 
-        return self.send('G28 XY') 
+    # def homeXY(self):
+    #     if self.test_mode:
+    #         self.current_position[0] = 0.0 
+    #         self.current_position[1] = 0.0 
+    #     return self.send('G28 XY') 
 
-    def setRel(self):
-        return self.send("G91") 
+    # def setRel(self):
+    #     return self.send("G91") 
 
-    def setAbs(self):
-        return self.send("G90")      
+    # def setAbs(self):
+    #     return self.send("G90")      
 
-    def moveRel(self, coordinate, speed=1000):
-        if self.test_mode:
-            self.current_position[0] = float(coordinate[0]) + self.current_position[0] 
-            self.current_position[1] = float(coordinate[1]) + self.current_position[1] 
-            self.current_position[2] = float(coordinate[2]) + self.current_position[2] 
-        self.setRel() 
-        line = self.send('G0 X{}  Y{}  Z{}  F{}'.format(coordinate[0], coordinate[1], coordinate[2], speed))
-        self.setAbs() 
-        return line
+    # def moveRel(self, coordinate, speed=1000):
+    #     if self.test_mode:
+    #         self.current_position[0] = float(coordinate[0]) + self.current_position[0] 
+    #         self.current_position[1] = float(coordinate[1]) + self.current_position[1] 
+    #         self.current_position[2] = float(coordinate[2]) + self.current_position[2] 
+    #     self.setRel() 
+    #     line = self.send('G0 X{}  Y{}  Z{}  F{}'.format(coordinate[0], coordinate[1], coordinate[2], speed))
+    #     self.setAbs() 
+    #     return line
 
-    def moveAbs(self, coordinate, speed=1000):
-        """
-        TODO: calculate move time
-        """
-        if self.test_mode:
-            self.current_position = coordinate 
+    # def moveAbs(self, coordinate, speed=1000):
+    #     """
+    #     TODO: calculate move time
+    #     """
+    #     if self.test_mode:
+    #         self.current_position = coordinate 
         
-        return self.send('G0 X{}  Y{}  Z{}  F{}'.format(coordinate[0], coordinate[1], coordinate[2], speed))
+    #     return self.send('G0 X{}  Y{}  Z{}  F{}'.format(coordinate[0], coordinate[1], coordinate[2], speed))
         
     def disconnect(self):
         if self.test_mode:
