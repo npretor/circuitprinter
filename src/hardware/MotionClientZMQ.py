ec21daf8c55206dd5e6ef1b8d1391b351128ecb8
npretorius <npretorius@nextflex.us>
2023-04-21 19:21:32 -0700

    created a MotionClient class and tested working connect and disconnect

--- src/hardware/MotionClientZMQ.py
+++ src/hardware/MotionClientZMQ.py
@@ -15,23 +15,21 @@ class MotionClient:
     """
 
     """
-    def __init__(self, address='127.0.0.1', socket_port=5555):
+    def __init__(self, port='/dev/ttyACM0', address='127.0.0.1', socket_port=5678):
+        self.port = port 
         context = zmq.Context()
         self.socket = context.socket(zmq.REQ)
         self.socket.connect("tcp://{}:{}".format(address, socket_port))  
 
-    # def connect(self, port="/dev/ttyACM0", baudrate=115200, test_mode=False):
-    #     message = {"connect": {"port": port, "baudrate": baudrate, "test_mode": test_mode}} 
-    #     res = self.send(message)
-    #     return res['result']
-
-    # def gcode(self, message):
-    #     res = self.send(message)
-    #     return res['result']
-
     def send(self, message):
         self.socket.send(message.encode())
         return(self.socket.recv().decode())
+    
+    def connect(self):
+        return self.send(json.dumps({'connect': True}) )
+
+    def disconnect(self):
+        return self.send(json.dumps({'connect': False}) )
 
     def close(self):
         self.socket.close()  
