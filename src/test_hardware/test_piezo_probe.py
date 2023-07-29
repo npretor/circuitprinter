37f6996cf9c0df9f0ace1f353c8f55cccc711226
npretorius <npretor@g.clemson.edu>
2023-07-28 21:09:03 -0700

    piezo hardware test comments

--- src/test_hardware/test_piezo_probe.py
+++ src/test_hardware/test_piezo_probe.py
@@ -10,6 +10,12 @@ printer.connect()
 
 time.sleep(5)
 
+# Probe the z zero 
 printer.piezoProbe(2)
+
+print("Moving to print height and location") 
+
+# All bed locations should be zero relative to the camera for registration purposes. 
+# All tools should also be registered relative to the downward camera 
 
 printer.disconnect() 
