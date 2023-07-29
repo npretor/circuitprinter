2f9a2bbe1d2dc150156b672731a1709f387d8b84
npretorius <npretor@g.clemson.edu>
2023-07-28 21:05:34 -0700

    extrusion print test function partial

--- src/Printer.py
+++ src/Printer.py
@@ -84,7 +84,8 @@ class Printer:
         6. Get the machine position at that point, and save. 
         7. Add the bed-to-probe offset and save 
 
-
+        The z difference between the bed and the z probe is 9.5mm. 
+        Moving +9.5 in the Z direction should get any tool to the bed height 
         Z 6.30 at the bed 
         Z -3.20 at the z probe location  
         bed - probe = 6.3 - -3.2
@@ -129,6 +130,8 @@ class Printer:
         position = self.motion.position() 
         logging.info(position) 
 
+        self.machine_settings["tools"][str(tool_number)]["tip_zero"] = position 
+
         # Raise up 
         self.motion.gcode('G1 Z3 F50') 
 
@@ -138,10 +141,97 @@ class Printer:
         # Unload tool 
         self.motion.gcode("T-1")   
 
-
     def hardwareSpecificSetup(self):
-        # Run machine-specific startup commands
+        '''Run machine-specific startup commands here'''
         self.motion.gcode('M302 P1')    # Allow cold extrudes
+
+    def easy_print(self, tool_number=2):
+        """
+        # Easy print of a test pattern
+        ### Z height calculations 
+        How is the Z location calculated? 
+        Machine zero is where the probe zeros out when it hits the bed. 
+        Heights: 
+        
+        Sum all of these: 
+        - Z Probe Offset (Height from the Z probe plane to the bed plane) 9.5mm 
+        - Substrate height (substrate thickness): 0.1mm default
+        - Print height (height between the substrate and the tip): 
+        
+        ## Printing processes(copied from the pressure print script )
+        For each polyline: 
+        1. Rapid to starting location on the PT0, at the rapid Z height 
+        2. Drop down to the Z height 
+        3. Pressure on 
+            1. Delay
+                1. Move to each point at speed, skipping the first point
+            2. Delay 
+        4. Pressure off 
+        5. Lift up to rapid height (might need to double back to prevent trailing) 
+        """
+        polylines = [
+            [[0,0],[0,10]],
+            [[2,0],[2,10]],
+            [[4,0],[4,10]],
+            [[6,0],[6,10]],
+            [[8,0],[8,10]]
+        ] 
+        # Rapid and origin 
+        bed_origin = self.machine_settings["printbed_origin"] 
+        rapid_height = self.machine_settings["rapid_height"] 
+        rapid_speed = self.machine_settings["rapid_speed"] 
+        rapid_speed_z = self.machine_settings["rapid_speed_z"] 
+
+        # Print height calculations 
+        z_probe_offset= self.machine_settings["probe_to_bed_z_offset"]
+        substrate_height = self.machine_settings["substrate_height"]
+        print_height = self.machine_settings["print_height"]                
+
+        tip_zero = self.machine_settings["tools"][str(tool_number)]["tip_zero"]
+        print_z_height = tip_zero = z_probe_offset + substrate_height + print_height
+
+        # Print parameters 
+        print_speed = self.machine_settings["print_speed"]
+        delay_time = 100 # milliseconds
+        kick = 1 #mm  
+        retract = 0.5
+
+        # = = = = = = = Start movement = = = = = = = # 
+        # Start location should be the print origin coordinate 
+        
+        # Rapid to printbed origin at rapid_height, rapid_speed 
+        self.motion.gcode('G0 X{} Y{} Z{} F{}'.format(bed_origin[0], bed_origin[0], rapid_height, rapid_speed))  
+
+        for polyline in polylines:
+            # Rapid to starting location, at rapid Z height 
+            self.motion.gcode('G0 X{} Y{} Z{} F1000'.format(polyline[0][0], polyline[0][1], rapid_height, rapid_speed))  
+
+            # Drop down to print height at z_rapid_speed
+            self.motion.gcode('G1 Z{} F{}'.format(print_z_height, rapid_speed_z))         
+
+            # Start extruding with a kick 
+            # self.motion.gcode('G1 E{}'.format(kick))  
+
+            # Delay 
+            #self.motion.gcode('G4 {}'.format(delay_time)) 
+
+            # Move to each successive point 
+            for point in polyline[1:]:
+                self.motion.gcode('G0 X{} Y{} F{}'.format(point[0],point[0], print_speed)) 
+
+            # Retract 
+            # self.motion.gcode('G1 E-{}'.format(retract))  
+            
+            # Raise up to rapid Z height 
+            self.motion.gcode('G0 Z{} F{}}'.format(rapid_height, rapid_speed))  
+
+
+
+            
+
+        
+
+
 
     # = = = = = = = = = = Process artwork and print = = = = = = = = = # 
     def parseDesign(self, filePath):
