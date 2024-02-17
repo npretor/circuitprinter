750729413bd0e0b304773e88e3147f67b12451c0
npretorius <npretor@g.clemson.edu>
2024-02-17 10:29:21 -0800

    config classes

---
+++ src/Configs.py
@@ -0,0 +1,45 @@
+from dataclasses import dataclass
+
+@dataclass 
+class Camera:
+    """Camera settings"""
+    name: str 
+    exposure: float 
+    shutter_speed: float 
+    
+    focus_distance: float 
+    pixels_per_millimeter: float 
+
+
+@dataclass 
+class Lights:
+    """Light settings"""
+    side_lights_on: bool = True 
+    ring_light_on: bool = False 
+
+
+
+class MatrixDeviceLayout:
+    def __init__(self, separation, width, offset=(0.0,0.0)) -> None:
+        self.offset = offset            # Offset if used
+        self.separation = separation    # Center to center 
+        self.width = width              # Device width 
+
+    def generate():
+        scan_locations = []
+        return scan_locations
+
+
+class ArbitraryLayout:
+    """
+    List of locations to go to
+    
+    """
+    def __init__(self, device_centroids, transforms, offset=(0.0,0.0)) -> None:
+        self.device_centroids = device_centroids        # Offset if used
+        self.offset = offset                            # offset from zero 
+        self.transforms = transforms                    # Not sure yet 
+    
+    def generate():
+        scan_locations = []
+        return scan_locations        
